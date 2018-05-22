bc4cbb4b8b170e253bc552881fe3374d934d762a
Kanshu Yokoo <kanshu@lotusandcompanyinc.com>
2018-05-23 08:53:32 +0900

    first commit

---
+++ KKModalStatus/view/KKModalStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  KKModalStatusView.swift
+//  KKModalStatus
+//
+//  Created by 観周 横尾 on 22/05/2018.
+//  Copyright © 2018 myself. All rights reserved.
+//
+
+import UIKit
+
+class KKModalStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
