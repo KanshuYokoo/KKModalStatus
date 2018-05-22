05a570cbae9aa0f7b372ba5b7faf2f39f9902232
Kanshu Yokoo <kanshu@lotusandcompanyinc.com>
2018-05-22 11:37:51 +0900

    Initial Commit

---
+++ KKModalStatus/KKModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  KKModalStatus.h
+//  KKModalStatus
+//
+//  Created by 観周 横尾 on 22/05/2018.
+//  Copyright © 2018 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKModalStatus.
+FOUNDATION_EXPORT double KKModalStatusVersionNumber;
+
+//! Project version string for KKModalStatus.
+FOUNDATION_EXPORT const unsigned char KKModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKModalStatus/PublicHeader.h>
+
+
---
+++ KKModalStatusTests/KKModalStatusTests.m
@@ -0,0 +1,39 @@
+//
+//  KKModalStatusTests.m
+//  KKModalStatusTests
+//
+//  Created by 観周 横尾 on 22/05/2018.
+//  Copyright © 2018 myself. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KKModalStatusTests : XCTestCase
+
+@end
+
+@implementation KKModalStatusTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
